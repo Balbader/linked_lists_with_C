f656615fdcdcc025e3140ba1953621fb988ab7ae
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-31 10:17:08 +0200

    insert_at_tail.c

---
+++ insert_at_tail.c
@@ -0,0 +1,17 @@
+#include "lstlib.h"
+
+t_list	*insert_at_tail(t_list *lst, int new_value)
+{
+	t_list	*new_node;
+	t_list	*current;
+
+	if (lst == NULL)
+		return (new_node);
+	new_node = calloc(1, sizeof(t_list));
+	new_node->content = new_value;
+	current = lst;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new_node;
+	return (lst);
+}
