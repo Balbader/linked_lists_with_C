a9b3416a089ae973a554ae40db821ae9a1c4e262
Balbader <15971200+Balbader@users.noreply.github.com>
2022-09-03 09:25:06 +0200

    delete_at_tail.c

---
+++ delete_at_tail.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	int				value;
+	struct s_list	*next;
+}				t_list;
+
+t_list	*delete_at_tail(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	else if (lst->next == NULL)
+	{
+		free(lst);
+		return (NULL);
+	}
+	else
+	{
+		t_list	*current;
+		t_list	*prev;
+
+		current = lst;
+		prev = NULL;
+		while (current->next != NULL)
+		{
+			prev = current;
+			current = current->next;
+		}
+		prev->next = NULL;
+		free(current);
+		return (lst);
+	}
+}
