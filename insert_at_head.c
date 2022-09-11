428fc30a16cb3fe6f5c811955531cddbefca7a3a
Balbader <15971200+Balbader@users.noreply.github.com>
2022-09-11 12:03:32 +0200

    insert_at_head.c

--- insert_at_head.c
+++ insert_at_head.c
@@ -7,6 +7,7 @@ t_list	*insert_at_head(t_list *lst, int new_value)
 	if (lst == NULL)
 		return (new_node);
 	new_node = calloc(1, sizeof(t_list));
+	new_node->content = new_value;
 	(*new_node).content = new_value;
 	new_node->next = lst;
 	return (new_node);
