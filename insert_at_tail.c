43b39ab55d0bf13b3f0c29836d4fd765be2963e6
Balbader <15971200+Balbader@users.noreply.github.com>
2022-09-11 12:08:39 +0200

    insert_at_tail.c

--- insert_at_tail.c
+++ insert_at_tail.c
@@ -5,10 +5,10 @@ t_list	*insert_at_tail(t_list *lst, int new_value)
 	t_list	*new_node;
 	t_list	*current;
 
+	new_node = NULL;
 	if (lst == NULL)
 		return (new_node);
 	new_node = calloc(1, sizeof(t_list));
-	new_node = NULL;
 	new_node->content = new_value;
 	current = lst;
 	while (current->next != NULL)
